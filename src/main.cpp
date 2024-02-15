881a29537fc16d6bfbb9bf75a59bd15721ffda7b
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-02-15 07:19:24 +0100

    Clean up #include in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,6 @@
 #include "article.h"
 #include "menu.h"
-#include "databaseQueries.h"
-#include "clothing.h"
-#include "books.h"
-#include "laptops.h"
 #include "menuFunction.h"
-#include <iomanip>
-#include <iostream>
-#include <memory>
-#include <any>
 #include <vector>
 
 
