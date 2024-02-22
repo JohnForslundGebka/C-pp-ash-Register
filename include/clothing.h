ccaa2fbd544a11d0e531da43ffc71e24ec490fb4
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-02-22 09:07:31 +0100

    Add comments

--- include/clothing.h
+++ include/clothing.h
@@ -1,3 +1,18 @@
+// ==============================================================================
+// clothing.h
+// Header file for the Clothing class
+//
+// The Clothing class represents clothing items in the inventory,
+//
+// Features:
+// - Inherits from Article to leverage common inventory item attributes.
+// Usage:
+// Utilized within the inventory management system for creating, updating,
+// and displaying information specific to laptop clothing items.
+//
+// Author: John Forslund Gebka
+// Date: 2024-02-01
+// ==============================================================================
 #ifndef C_PP_ASH_REGISTER_CLOTHING_H
 #define C_PP_ASH_REGISTER_CLOTHING_H
 #include <string>
@@ -11,6 +26,7 @@ private:
     std::string m_color{};
     const std::string m_table{"clothes_inventory"};
 public:
+    //constructor
     Clothing(std::string title, int artNumber, double purchasePrice, double retailPrice, int amount, std::string table, std::string size, std::string color, std::string brand);
     explicit Clothing(int artNumber);
 
