a1ef11647b4b20d6498d22a8a344c14402560d12
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-02-22 08:36:03 +0100

    add comments

--- include/databaseQueries.h
+++ include/databaseQueries.h
@@ -1,6 +1,33 @@
+// ==============================================================================
+// DatabaseQueries.h
+// Header file for the DatabaseQueries class.
+//
+// The DatabaseQueries class provides an abstraction layer for all database interactions,
+// encapsulating the functionality required to query and update the SQLite database.
+// This includes operations such as retrieving article information and updating stock levels.
+// The class utilizes SQLiteCpp, a C++ wrapper for SQLite, to manage database connections
+// and execute SQL statements
+//
+// Features:
+// - Manages a connection to the SQLite database using SQLiteCpp.
+// - Provides methods to fetch data from the database based on article number and table name,
+//   supporting dynamic retrieval of information for different types of inventory items.
+// - Offers functionality to update the stock amount in the database, facilitating the sale
+//   and restocking processes.
+//
+//
+// Example:
+// DatabaseQueries dbQueries;
+// auto statement = dbQueries.getDataFromDb(articleNumber, "laptops_inventory");
+// if (statement && statement->executeStep()) {
+//     // Process the retrieved data
+// }
+//
+// Author: John Forslund Gebka
+// Date: 2024-02-01
+// ==============================================================================
 #ifndef C_PP_ASH_REGISTER_DATABASEQUERIES_H
 #define C_PP_ASH_REGISTER_DATABASEQUERIES_H
-
 #include <SQLiteCpp/SQLiteCpp.h>
 #include <iostream>
 #include <string>
@@ -14,10 +41,8 @@ private:
 public:
     DatabaseQueries();
 
-    //functions that creates a SQL query for that type of object
+    //Member functions that creates a SQL query for that type of object
     std::unique_ptr<SQLite::Statement> getDataFromDb(int articleNumber,const std::string &table);
-
-
     bool updateAmountInDb(int articleNumber,const std::string &table, int newAmount);
 
 
--- include/laptops.h
+++ include/laptops.h
@@ -1,3 +1,22 @@
+// ==============================================================================
+// laptops.h
+// Header file for the Laptops class
+//
+// The Laptops class represents laptop items in the inventory, extending the generic
+// Article class to include specific attributes relevant to laptops, such as brand,
+// RAM, CPU, and hard drive specifications.
+//
+// Features:
+// - Inherits from Article to leverage common inventory item attributes.
+// Usage:
+// Utilized within the inventory management system for creating, updating,
+// and displaying information specific to laptop inventory items. The Laptops
+// class integrates seamlessly with the database for persistent storage and retrieval
+// of item details.
+//
+// Author: John Forslund Gebka
+// Date: 2024-02-01
+// ==============================================================================
 #ifndef C_PP_ASH_REGISTER_LAPTOPS_H
 #define C_PP_ASH_REGISTER_LAPTOPS_H
 #include "databaseQueries.h"
@@ -10,7 +29,7 @@ private:
       std::string m_ram{};
       std::string m_cpu{};
       std::string m_hardDrive{};
-    const std::string m_table{"laptops_inventory"};
+    const std::string m_table{"laptops_inventory"}; //table in the database
 public:
   Laptops() : m_brand(), m_ram(), m_cpu(), m_hardDrive() {} // default constructor
   explicit Laptops(int articleNumber);
